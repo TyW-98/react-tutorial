f47842b2ee8e0dc99cd7bc3cfa0795e74a5ab390
wey1998 <yitwey1998@hotmail.com>
2023-05-18 05:42:25 +0100

    Add interests component

--- static-page/business-card/src/App.jsx
+++ static-page/business-card/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Info from "./components/Info";
 import About from "./components/About";
+import Interest from "./components/Interest";
 
 export default function App() {
   return (
     <>
       <Info />
       <About />
+      <Interest />
     </>
   );
 }
