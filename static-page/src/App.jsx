01dbfd6b5e10d34bd5e57334c04c58cb7764c817
wey1998 <yitwey1998@hotmail.com>
2023-05-18 02:10:41 +0100

    Add styling to navbar

--- static-page/src/App.jsx
+++ static-page/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ static-page/src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <div className="main-container">
+      <h1>The Main content goes here</h1>
+    </div>
+  );
+}
