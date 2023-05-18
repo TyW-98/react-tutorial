c3a54ea1f434419a927f1f7b130b678107495858
wey1998 <yitwey1998@hotmail.com>
2023-05-18 05:37:16 +0100

    Add About component

--- static-page/business-card/src/App.jsx
+++ static-page/business-card/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Info from "./components/Info";
+import About from "./components/About";
 
 export default function App() {
   return (
     <>
       <Info />
+      <About />
     </>
   );
 }
