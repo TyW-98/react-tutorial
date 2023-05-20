d50dbc2d51007b061c7e39c52a6cd1e5ee6179f5
wey1998 <yitwey1998@hotmail.com>
2023-05-20 03:08:03 +0100

    Add functionality to button so when clicked it logs the image url randomly taken from the data

--- interactive-page/project-1/src/components/Form.jsx
+++ interactive-page/project-1/src/components/Form.jsx
@@ -1,11 +1,22 @@
-export default function Form() {
+import data from "../data";
+
+export default function Form(props) {
+  function getRandomNumber() {
+    return Math.floor(Math.random() * data.data.memes.length);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(data.data.memes[getRandomNumber()].url);
+  }
+
   return (
     <main>
       <div className="form-container">
         <form className="form-main">
           <input type="text" />
           <input type="text" />
-          <button type="submit" className="submit-btn">
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
             Get a new Image 🖼
           </button>
         </form>
