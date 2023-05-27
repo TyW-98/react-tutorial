7b4d9ba301d909bed05598d8ddda445246ed8873
wey1998 <yitwey1998@hotmail.com>
2023-05-27 23:42:29 +0100

    Add Header component

--- Tenzies-app/src/App.jsx
+++ Tenzies-app/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div>
-      <h1>Testing </h1>
+    <div className="outer-container">
+      <div className="inner-container">
+        <Header />
+      </div>
     </div>
   );
 }
---
+++ Tenzies-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h2>Tenzies</h2>
+      <h5>
+        Roll until all dice are the same. Click each dice to freeze it at its
+        current value between rolls.
+      </h5>
+    </div>
+  );
+}
