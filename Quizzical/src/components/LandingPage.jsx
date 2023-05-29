45aa094c701d33b6d35600e92f0d7414f9da38f5
wey1998 <yitwey1998@hotmail.com>
2023-05-29 01:24:24 +0100

    Add quizzical landing page

---
+++ Quizzical/src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+export default function LandingPage() {
+  return (
+    <div className="landing-page-container">
+      <h1>Quizzical</h1>
+      <h5>
+        Ignite your intellect with Quizzical's single-player quizzes and embark
+        on a solo journey of knowledge and fun.
+      </h5>
+      <button type="submit" className="start-btn">
+        Start Quiz
+      </button>
+    </div>
+  );
+}
