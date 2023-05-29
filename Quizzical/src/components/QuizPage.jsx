34e275be8e697d9f8ddd9f031ae7eda1c949c87d
wey1998 <yitwey1998@hotmail.com>
2023-05-29 05:28:50 +0100

    Display BeatLoader while fetching data from API

--- Quizzical/src/components/QuizPage.jsx
+++ Quizzical/src/components/QuizPage.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import QuizCard from "./QuizCard";
 import { nanoid } from "nanoid";
+import BeatLoader from "react-spinners/BeatLoader";
 
 export default function QuizPage(props) {
   const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
@@ -16,7 +18,8 @@ export default function QuizPage(props) {
             options: shuffleArray([...q.incorrect_answers, q.correct_answer]),
           }))
         );
-      });
+      })
+      .then(() => setLoading(false));
   }, []);
 
   function handleCheckAnswers(event) {
@@ -31,25 +34,33 @@ export default function QuizPage(props) {
     return array;
   }
 
-  console.log(questions[0].correct_answer);
-
   return (
-    <div className="quiz-container">
-      <h5 className="back-btn" onClick={props.handleStartBtn}>
-        ← Back
-      </h5>
-      {questions.map((q) => (
-        <QuizCard key={q.id} question={q.question} answers={q.options} />
-      ))}
-      <div className="check-answers-btn-container">
-        <button
-          type="button"
-          className="check-answers-btn"
-          onClick={(event) => handleCheckAnswers(event)}
-        >
-          Check answers
-        </button>
-      </div>
+    <div>
+      {loading ? (
+        <div className="loading-container">
+          <div className="loading-component">
+            <BeatLoader color="#2019a8" size={25} speedMultiplier={0.6} />
+          </div>
+        </div>
+      ) : (
+        <div className="quiz-container">
+          <h5 className="back-btn" onClick={props.handleStartBtn}>
+            ← Back
+          </h5>
+          {questions.map((q) => (
+            <QuizCard key={q.id} question={q.question} answers={q.options} />
+          ))}
+          <div className="check-answers-btn-container">
+            <button
+              type="button"
+              className="check-answers-btn"
+              onClick={(event) => handleCheckAnswers(event)}
+            >
+              Check answers
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
