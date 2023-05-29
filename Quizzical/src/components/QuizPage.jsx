44161d75785207fac12dc0481e600c24a42ddeac
wey1998 <yitwey1998@hotmail.com>
2023-05-29 03:06:10 +0100

    Add back button to return back to landing page

--- Quizzical/src/components/QuizPage.jsx
+++ Quizzical/src/components/QuizPage.jsx
@@ -1,8 +1,9 @@
 import QuizCard from "./QuizCard";
 
-export default function QuizPage() {
+export default function QuizPage(props) {
   return (
-    <div className="quiz-container">
+    <div className="quiz-container" onClick={props.handleStartBtn}>
+      <h5 className="back-btn">← Back</h5>
       <QuizCard />
       <div className="check-answers-btn-container">
         <button type="button" className="check-answers-btn">
